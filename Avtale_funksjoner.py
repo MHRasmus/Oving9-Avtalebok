9fbcc4d2bedb68e4052efb6204d3d3cea6db8212
MHRasmus <mhr1701@gmail.com>
2022-10-19 22:02:25 +0200

    Update Avtale_funksjoner.py
    Funksjonell ny avtale funksjon

--- Avtale_funksjoner.py
+++ Avtale_funksjoner.py
@@ -5,23 +5,33 @@ from datetime import datetime
 def ny_avtale():
     #Lager et dictionary for lagring av avtalene
     avtale_dict = dict()
-    try:
-        valgt_tittel = input("Skriv inn tittel: ")
-        valgt_sted = input("Skriv inn sted: ")
-    except TypeError:
-        print("Skriv inn gyldig ord")
 
-    try:
-        valgt_starttidspunkt = datetime.strptime(input("Skriv inn starttidspunkt(ÅÅÅÅ-MM-DD,TT:MM:SS): "), "%Y-%m-%d %H:%M:%S")
-        formatert_tid = datetime.isoformat(valgt_starttidspunkt)
-    except ValueError:
-        print("Feil format på innskrevet tidspunkt")
+    #Bruker skriver inn avtaledeljer
+    valgt_tittel = str(input("Skriv inn tittel: "))
+    valgt_sted = str(input("Skriv inn sted: "))
 
-    try:
-        valgt_varighet = int(input("Skriv inn varighet i minutter: "))
-    except ValueError:
-        print("Skriv inn en gyldig varighet i minutter")
-   
+    #Sjekker om brukervalgt tidspunkt er i gyldig format (ÅÅÅÅ-MM-DD TT:MM)
+    while True:
+        try:
+            valgt_starttidspunkt = datetime.strptime(input("Skriv inn starttidspunkt(ÅÅÅÅ-MM-DD TT:MM): "), "%Y-%m-%d %H:%M")
+            break
+        except ValueError:
+            print("***Feil format på tidspunkt***")
+            continue
+
+    #Sjekker om brukervalgt varighet er i gyldig format (int)
+    while True:
+        try:
+            valgt_varighet = int(input("Skriv inn varighet i hele minutter: "))
+            break
+        except ValueError:
+            print("Feil format på varighet")
+            continue
+
+    #Lager dictionary med valgte data
+    avtale_dict[valgt_tittel] = Avtalebok(valgt_tittel,valgt_sted,valgt_starttidspunkt,valgt_varighet)
+
+    print(avtale_dict[valgt_tittel])
 
 
 ny_avtale()
