f1dddef0139128ef7607d8b17a25440596d0ff62
MHRasmus <mhr1701@gmail.com>
2022-10-25 12:17:03 +0200

    Update Avtalebok.py
    Endret på print setninger

--- Avtalebok.py
+++ Avtalebok.py
@@ -6,10 +6,10 @@ avtalebok_dict = dict()
 valg = Af.menyvalg()
 while valg != 9:
     if valg == 1:
-        fil = input("Skriv inn navnet på filen som skall leses inn: ")
+        fil = input("Skriv inn navnet på filen som du vil lese inn avtaler fra: ")
         Af.henter_avtalebok(avtalebok_dict, fil)
     elif valg == 2:
-        fil = input("Skriv inn navnet på filen som avtalene skall skrives til: ")
+        fil = input("Skriv inn navnet på filen som avtalene skal skrives til: ")
         Af.lagrer_dict(avtalebok_dict, fil)
     elif valg == 3:
         Af.ny_avtale(avtalebok_dict)
@@ -18,16 +18,16 @@ while valg != 9:
     elif valg == 5:
         Af.print_avtale(avtalebok_dict)
         try:
-            print("\nFor å komme ut av denne menyen uten å slette noe. "
+            print("\nFor å komme ut av denne menyen uten å slette noe, "
                   "Skriv noe som ikke er navnet til noen av avtalene.")
-            slett = input("Skriv inn navnet på avtalen du vill slette: ")
+            slett = input("Skriv inn navnet på avtalen du vil slette: ")
             avtalebok_dict.pop(slett)
         except KeyError:
             print("Finner ikke en avtale med det navnet.")
     elif valg == 6:
         Af.print_avtale(avtalebok_dict)
         try:
-            rediger = input("\nSkriv inn navnet på avtalen du vill redigere: ")
+            rediger = input("\nSkriv inn navnet på avtalen du vil redigere: ")
             print(f"{avtalebok_dict[rediger]}\n")
             Af.rediger_avtale(avtalebok_dict, rediger)
         except KeyError:
