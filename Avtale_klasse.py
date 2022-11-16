f3077ee09079c12c84d879ae5d1005fc2583703e
ruben-t-pinheiro <ruben.t.pinheiro@gmail.com>
2022-11-16 14:28:59 +0100

    skrev om oppgaven sånn att sted og kategori klassene brukes i avtalen
    skrev om oppgaven sånn att sted og kategori klassene brukes i avtalen
    
    gjenstår å testes grundig å sørge for at den besvarer alle delene av oppgaven
    spesielt usikker på om den besvarer n) med det at den skall fungere uten adresse

--- Avtale_klasse.py
+++ Avtale_klasse.py
@@ -1,21 +1,26 @@
 
-#Definerer klassen Avtalebok
+# Definerer klassen Avtalebok
 class Avtalebok:
-    #Definerer variablene i klassen
-    def __init__(self,tittel="",sted=list(),starttidspunkt=0,varighet=0,kategori=""):
+    # Definerer variablene i klassen
+    def __init__(self, tittel="", sted="", starttidspunkt=0, varighet=0, kategori=list()):
         self.tittel = tittel
         self.sted = sted
         self.starttidspunkt = starttidspunkt
         self.varighet = varighet
         self.kategori = kategori
 
+    def legg_til_kategori(self, kategori):
+        self.kategori.append(kategori)
+
     def __str__(self):
-        return f"\nTittel: {self.tittel}\nSted: {self.sted}\nStarttidspunkt: {self.starttidspunkt}\nVarighet: {self.varighet}\nKategori: {str(self.kategori)}"
+        return f"\nTittel: {self.tittel}\nSted: {(str(self.sted))}\nStarttidspunkt: {self.starttidspunkt}" \
+               f"\nVarighet: {self.varighet}\nKategori: {' '.join(map(str, self.kategori))}"
+
     def __repr__(self):
-        return f"\nTittel: {self.tittel},Sted: {self.sted},Starttidspunkt: {self.starttidspunkt},Varighet: {self.varighet}"
+        return f"{self.tittel};{repr(self.sted)};{self.starttidspunkt};{self.varighet};{repr(self.kategori)}"
 
 
-#Definerer klassen Kategori
+# Definerer klassen Kategori
 class Kategori:
     def __init__(self, id, navn, prioritet="Vanlig"):
         self.id = id
@@ -23,16 +28,21 @@ class Kategori:
         self.prioritet = prioritet
 
     def __str__(self):
-        return f"\nId: {self.id} \nNavn: {self.navn} \nPrioritet: {self.prioritet}"
+        return f"\n  Id: {self.id} \n  Navn: {self.navn} \n  Prioritet: {self.prioritet}"
+
+    def __repr__(self):
+        return f"{self.id},{self.navn},{self.prioritet}: "
 
 
+# Definerer klassen Sted
 class Sted:
     def __init__(self, id, navn, adresse):
         self.id = id
         self.navn = navn
         self.adresse = adresse
-    
+
     def __str__(self):
-        return f"\nId: {self.id}\nNavn: {self.navn}\nAdresse: {self.adresse}"
+        return f"\n  Id: {self.id}\n  Navn: {self.navn}\n  Adresse: {self.adresse}"
 
-
+    def __repr__(self):
+        return f"{self.id}:{self.navn}:{self.adresse}"
--- Avtalebok.py
+++ Avtalebok.py
@@ -6,26 +6,28 @@ sted_fil = "Steder.txt"
 
 avtalebok_dict = dict()
 kategori_dict = dict()
-kategori_liste = list()
 sted_dict = dict()
-sted_liste = list()
 
-fil_hentet = False
+#fil_hentet = False
 
-Af.henter_avtalebok(avtalebok_dict, avtale_fil,kategori_dict,kategori_fil,kategori_liste,sted_dict,sted_fil,sted_liste)
+#Af.henter_avtalebok(avtalebok_dict, avtale_fil)
+#Af.henter_kategorier(kategori_dict, kategori_fil)
+#Af.henter_sted(sted_dict, sted_fil)
 
 valg = Af.menyvalg()
-while valg != 9:
-    fil_hentet_status = fil_hentet
+while valg != 13:
+    #fil_hentet_status = fil_hentet
     if valg == 1:
-        Af.henter_avtalebok(avtalebok_dict,avtale_fil,kategori_dict,kategori_fil,kategori_liste,sted_dict,sted_fil,sted_liste)
-        fil_hentet = True
+        Af.henter_avtalebok(avtalebok_dict, avtale_fil)
+        Af.henter_kategorier(kategori_dict, kategori_fil)
+        Af.henter_sted(sted_dict, sted_fil)
+        #fil_hentet = True
     elif valg == 2:
-        Af.ny_avtale(avtalebok_dict,sted_dict,sted_liste,kategori_dict,kategori_liste)
+        Af.ny_avtale(avtalebok_dict, sted_dict, kategori_dict)
     elif valg == 3:
-        Af.print_avtale(avtalebok_dict)
+        Af.print_dictonary(avtalebok_dict, "Avtaler:")
     elif valg == 4:
-        Af.print_avtale(avtalebok_dict)
+        Af.print_dictonary(avtalebok_dict, "Avtaler:")
         try:
             print("\nFor å komme ut av denne menyen uten å slette noe, "
                   "Skriv noe som ikke er navnet til noen av avtalene.")
@@ -34,36 +36,39 @@ while valg != 9:
         except KeyError:
             print("Finner ikke en avtale med det navnet.")
     elif valg == 5:
-        Af.print_avtale(avtalebok_dict)
+        Af.print_dictonary(avtalebok_dict, "Avtaler:")
         try:
             rediger = input("\nSkriv inn navnet på avtalen du vil redigere: ")
             print(f"{avtalebok_dict[rediger]}\n")
-            Af.rediger_avtale(avtalebok_dict, rediger,sted_dict,sted_liste,kategori_dict,kategori_liste)
+            Af.rediger_avtale(avtalebok_dict, rediger, sted_dict, kategori_dict)
         except KeyError:
             print("Finner ikke en avtale med det navnet.")
     elif valg == 6:
-        #avtale_fil = input("Skriv inn navnet på filen som avtalene skal lagres i #NB! husk .txt#: ")
-        Af.lagrer_dict(avtalebok_dict,avtale_fil,kategori_dict,kategori_fil,sted_dict,sted_fil)
+        Af.print_dictonary(kategori_dict, "Lagrede kategorier:")
+        Af.ny_kategori(kategori_dict)
     elif valg == 7:
-        Af.ny_kategori(kategori_dict,kategori_liste)
+        Af.print_dictonary(kategori_dict, "Kategorier:")
     elif valg == 8:
-        Af.ny_sted(sted_dict,sted_liste)
+        try:
+            Af.legg_til_extra_kategori(avtalebok_dict, kategori_dict)
+        except ValueError:
+            print("Id på kategori må være et heltall")
+        #except KeyError:
+            #print("Finner ikke en avtale med det navnet.")
+    elif valg == 9:
+        Af.print_dictonary(sted_dict, "Lagrede steder:")
+        Af.ny_sted(sted_dict)
     elif valg == 10:
-        Af.lagrer_dict(avtalebok_dict, avtale_fil, kategori_dict, kategori_fil, sted_dict, sted_fil)
-        # Printer alle lagrete steder:
-        Af.print_dictonary(sted_dict, "Eksisterende steder")
-        søkeord = str(input("Skriv inn id-en på stedet du vil finne tilhørende avtaler til: "))
-        Af.søk_etter_sted(avtalebok_dict,avtale_fil,søkeord)
-
+        Af.print_dictonary(sted_dict, "Steder:")
     elif valg == 11:
-        Af.print_avtale(kategori_dict)
+        Af.print_dictonary(sted_dict, "Eksisterende steder:")
+        id_sted = int(input("Skriv inn id-en på stedet du vil finne tilhørende avtaler til: "))
+        søk_liste = Af.søk_etter_sted(avtalebok_dict, id_sted)
+        print('\n'.join(map(str, søk_liste)))
     elif valg == 12:
-        Af.print_avtale(sted_dict)
-
-
-
-
-
+        Af.lagrer_dict(avtalebok_dict, avtale_fil)
+        Af.lagrer_dict(kategori_dict, kategori_fil)
+        Af.lagrer_dict(sted_dict, sted_fil)
     else:
         pass
     valg = Af.menyvalg()
